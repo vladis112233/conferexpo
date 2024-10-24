fbc632b6b15586b72d39878f5fb5d8110fdb4297
vladis112233 <124692818+vladis112233@users.noreply.github.com>
2024-10-24 13:11:49 +0300

    15

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  // получаем все элементы с классом pushmenu
   const pushmenu = document.getElementsByClassName('pushmenu');
 
-  // получаем элемент с классом hidden-overley
   const hiddenOverley = document.querySelector('.hidden-overley');
 
-  // отслеживаем клик клика по оверлею
   hiddenOverley.addEventListener('click', (e) => {
     e.currentTarget.classList.toggle('show');
     document.querySelector('.sidebar').classList.toggle('show');
@@ -23,7 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.toggle('sidebar-opened')
   };
 
-  // Отслеживаем клики кнопок с классом pushmenu
   for( i=0; i < pushmenu.length; i++ ){
     pushmenu[i].addEventListener('click', pushmenuFunction, false);
   }
@@ -33,102 +29,48 @@ document.addEventListener('DOMContentLoaded', () => {
     this.parentNode.querySelector('ul').classList.toggle('show');
     this.querySelector('i').classList.toggle('rotate');
   }
-  // Отслеживаем клики родительских пунктов меню
   for( i=0; i < sidebarAccordeon.length; i++ ){
     sidebarAccordeon[i].addEventListener('click', accordeonFunction, false);
   }
 });
 
-// const carousel = document.querySelector('.carousel');
-// const slides = carousel.children;
-// const totalSlides = slides.length;
-// const visibleSlides = 3; // Количество видимых слайдов
-// let index = 0;
-//
-// // Функция для перемещения карусели
-// function moveCarousel() {
-//   index += visibleSlides; // Увеличиваем индекс на количество видимых слайдов
-//
-//   // Если индекс превышает общее количество слайдов, возвращаемся к первому
-//   if (index >= totalSlides) {
-//     index = 0; // Возвращаемся к первому слайду
-//   }
-//
-//   // Двигаем карусель
-//   carousel.style.transform = `translateX(${-index * 300}px)`; // Сдвигаем на ширину слайдов
-// }
-//
-// // Меняем слайды каждые 3 секунды
-// setInterval(moveCarousel, 10000);
-//
-
-
-// const carousel = document.querySelector('.carousel');
-// const slides = carousel.children;
-// const totalSlides = slides.length;
-// const visibleSlides = 4; // Количество видимых слайдов
-// let index = 0;
-//
-// // Функция для перемещения карусели
-// function moveCarousel() {
-//   index++; // Увеличиваем индекс на 1
-//
-//   // Если индекс превышает количество слайдов, возвращаемся к первому
-//   if (index >= totalSlides) {
-//     index = 0; // Возвращаемся к первому слайду
-//   }
-//
-//   // Двигаем карусель
-//   carousel.style.transform = `translateX(${-index * (300 / visibleSlides)}px)`; // Сдвигаем на ширину одного слайда
-// }
-//
-// // Меняем слайды каждые 3 секунды
-// setInterval(moveCarousel, 1000);
 
 const carousel = document.querySelector('.carousel');
 const slides = carousel.children;
 const totalSlides = slides.length;
-const visibleSlides = 3; // Количество видимых слайдов
+const visibleSlides = 3;
 let index = 0;
 
-// Функция для перемещения карусели
 function moveCarousel() {
   index++;
 
-  // Если индекс превышает количество слайдов - возвращаемся к первому
   if (index >= totalSlides - visibleSlides + 1) {
-    index = 0; // Возвращаемся к первому слайду
+    index = 0;
   }
 
-  // Двигаем карусель на один слайд
   carousel.style.transform = `translateX(${-index * 300}px)`; // Сдвигаем на ширину одного слайда
 }
 
-// Меняем слайды каждые 3 секунды
 setInterval(moveCarousel, 3000);
 
 
 
-const carousel1 = document.querySelector('.carousel1'); // Новый карусель
-const slides1 = carousel1.children; // Слайды второго каруселя
-const totalSlides1 = slides1.length; // Общее количество слайдов
-const visibleSlides1 = 4; // Количество видимых слайдов (измените на 5, если хотите)
-let index1 = 0; // Индекс для второго каруселя
+const carousel1 = document.querySelector('.carousel1');
+const slides1 = carousel1.children;
+const totalSlides1 = slides1.length;
+const visibleSlides1 = 4;
+let index1 = 0;
 
-// Функция для перемещения карусели в обратном направлении
 function moveCarousel1() {
-  index1--; // Уменьшаем индекс для движения назад
+  index1--;
 
-  // Если индекс меньше 0, возвращаемся к последнему слайду
   if (index1 < 0) {
-    index1 = totalSlides1 - visibleSlides1; // Устанавливаем индекс на последний видимый слайд
+    index1 = totalSlides1 - visibleSlides1;
   }
 
-  // Двигаем карусель
-  carousel1.style.transform = `translateX(${-index1 * 300}px)`; // Сдвигаем на ширину слайдов
+  carousel1.style.transform = `translateX(${-index1 * 300}px)`;
 }
 
-// Меняем слайды каждые 6 секунд
 setInterval(moveCarousel1, 3000);
 
 
@@ -136,72 +78,61 @@ setInterval(moveCarousel1, 3000);
 const carousel3 = document.querySelector('.carousel3');
 const slides3 = carousel3.children;
 const totalSlides3 = slides3.length;
-const visibleSlides3 = 2; // Количество видимых слайдов
+const visibleSlides3 = 2;
 let index3 = 0;
 
-// Функция для перемещения карусели
 function moveCarousel3() {
   index3++;
 
-  // Если индекс превышает количество слайдов - возвращаемся к первому
   if (index3 >= totalSlides3 - visibleSlides3 + 1) {
-    index3 = 0; // Возвращаемся к первому слайду
+    index3 = 0;
   }
 
-  // Двигаем карусель на один слайд
   carousel3.style.transform = `translateX(${-index3 * 300}px)`; // Сдвигаем на ширину одного слайда
 }
 
-// Меняем слайды каждые 3 секунды
 setInterval(moveCarousel3, 3000);
 
 
 
 
-const carousel4 = document.querySelector('.carousel4'); // Новый карусель
-const slides4 = carousel4.children; // Слайды второго каруселя
-const totalSlides4 = slides4.length; // Общее количество слайдов
-const visibleSlides4 = 5; // Количество видимых слайдов
-let index4 = 0; // Индекс для второго каруселя
+const carousel4 = document.querySelector('.carousel4');
+const slides4 = carousel4.children;
+const totalSlides4 = slides4.length;
+const visibleSlides4 = 5;
+let index4 = 0;
 
-// Функция для перемещения карусели в обратном направлении
 function moveCarousel4() {
-  index4--; // Уменьшаем индекс для движения назад
+  index4--;
 
-  // Если индекс меньше 0, возвращаемся к последнему слайду
   if (index4 < 0) {
-    index4 = totalSlides4 - visibleSlides4; // Устанавливаем индекс на последний видимый слайд
+    index4 = totalSlides4 - visibleSlides4;
   }
 
-  // Двигаем карусель
-  carousel4.style.transform = `translateX(${-index4 * 300}px)`; // Сдвигаем на ширину слайдов
+  carousel4.style.transform = `translateX(${-index4 * 300}px)`;
 }
 
-// Меняем слайды каждые 3 секунды
 setInterval(moveCarousel4, 3000);
 
 
 
-const carousel5 = document.querySelector('.carousel5'); // Находим элемент новой карусели
+const carousel5 = document.querySelector('.carousel5');
 const slides5 = carousel5.children;
 const totalSlides5 = slides5.length;
-const visibleSlides5 = 3; // Количество видимых слайдов
+const visibleSlides5 = 3;
 let index5 = 0;
 
-// Функция для перемещения новой карусели
 function moveCarousel5() {
   index5++;
 
-  // Если индекс превышает количество слайдов, возвращаемся к первому
   if (index5 >= totalSlides5 - visibleSlides5 + 1) {
-    index5 = 0; // Возвращаемся к первому слайду
+    index5 = 0;
   }
 
-  // Двигаем карусель на один слайд
-  carousel5.style.transform = `translateX(${-index5 * 300}px)`; // Сдвигаем на ширину одного слайда
+  carousel5.style.transform = `translateX(${-index5 * 300}px)`;
 }
 
-// Меняем слайды каждые 3 секунды
+
 setInterval(moveCarousel5, 3000);
 
 
@@ -210,15 +141,15 @@ setInterval(moveCarousel5, 3000);
 
 document.querySelectorAll('.a-li-under-menu').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
-    e.preventDefault(); // Предотвращаем стандартное поведение якорной ссылки
+    e.preventDefault();
 
-    const targetId = this.getAttribute('href'); // Получаем значение атрибута href
-    const targetElement = document.querySelector(targetId); // Находим элемент на странице
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
 
     if (targetElement) {
       window.scrollTo({
-        top: targetElement.offsetTop, // Позиция элемента на странице
-        behavior: 'smooth' // Плавная прокрутка
+        top: targetElement.offsetTop,
+        behavior: 'smooth'
       });
     }
   });
@@ -229,15 +160,15 @@ document.querySelectorAll('.a-li-under-menu').forEach(anchor => {
 
 document.querySelectorAll('#menu-main-menu a').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
-    e.preventDefault(); // Останавливаем стандартное поведение ссылки
+    e.preventDefault();
 
-    const targetId = this.getAttribute('href'); // Извлекаем ID целевого элемента
-    const targetElement = document.querySelector(targetId); // Находим этот элемент на странице
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
 
     if (targetElement) {
       window.scrollTo({
-        top: targetElement.offsetTop, // Получаем позицию элемента
-        behavior: 'smooth' // Устанавливаем плавную прокрутку
+        top: targetElement.offsetTop,
+        behavior: 'smooth'
       });
     }
   });
@@ -245,15 +176,15 @@ document.querySelectorAll('#menu-main-menu a').forEach(anchor => {
 
 
 document.querySelector('.button-to-contact .link-to-contact').addEventListener('click', function (e) {
-  e.preventDefault(); // Останавливаем стандартное поведение ссылки
+  e.preventDefault();
 
-  const targetId = this.getAttribute('href'); // Извлекаем значение атрибута href
-  const targetElement = document.querySelector(targetId); // Находим целевой элемент по id
+  const targetId = this.getAttribute('href');
+  const targetElement = document.querySelector(targetId);
 
   if (targetElement) {
     window.scrollTo({
-      top: targetElement.offsetTop, // Позиция целевого элемента на странице
-      behavior: 'smooth' // Плавная прокрутка
+      top: targetElement.offsetTop,
+      behavior: 'smooth'
     });
   }
 });
